d3773346634bf2919abc927b3cd82aab39b2e9f4
AdriROBOTICS <adriyabe@gmail.com>
2025-12-23 19:56:32 +0100

    Remove the answers

--- retos_python_microbit/AdriRobotics_Answers/Modulo_1/01.py
+++
@@ -1,7 +0,0 @@
-# Imports go at the top
-from microbit import *
-
-# Code in a 'while True:' loop repeats forever
-display.show(Image.PACMAN)
-sleep(1000)
-display.show("Hello Adri Y")
--- retos_python_microbit/AdriRobotics_Answers/Modulo_1/02.py
+++
@@ -1,4 +0,0 @@
-# Imports go at the top
-from microbit import *
-
-display.show("Hello Adri Y")
