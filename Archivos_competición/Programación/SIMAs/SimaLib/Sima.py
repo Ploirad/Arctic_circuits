d901c7577721d7a17ceff2bb2ca15b4905c49c3a
Darío <148375115+Ploirad@users.noreply.github.com>
2026-04-17 09:50:04 +0200

    Update Sima.py

--- Archivos_competición/Programación/SIMAs/SimaLib/Sima.py
+++ Archivos_competición/Programación/SIMAs/SimaLib/Sima.py
@@ -1,6 +1,5 @@
 from microbit import *
 import utime
-import math
 from Button import Button
 from wukong import *
 from RadioLib import Radio as r
@@ -12,7 +11,6 @@ class Sima:
         self.emergencyBtn = Button(panic_pin)
         self.radio = r(channel, Power=7)
         self.servo_pin = servo_pin
-
     def decode(self, message):
         # MESSAGE STRUCTURE:
         # "Color,Start"
@@ -26,27 +24,36 @@ class Sima:
         except ValueError:
             print("Invalid message format:", message)
             return False, False
-        
+
+    def _current_time(self):
+        """Tiempo actual en segundos (float)."""
+        return utime.ticks_ms() / 1000.0
+         
     def setColor(self, color):
         self.color = color
         
-        
+    def _adelante(self, der, izq):
+        self.wk.set_motors(1, izq)
+        self.wk.set_motors(2, der)
+
     def start(self):
         display.show(Image.ARROW_N)
         color = 0
         parar = True
         color, start = self.decode(self.radio.receive())
         while not start:
-            color, start = self.decode(self.radio.receive())
+           color, start = self.decode(self.radio.receive())
 
         self.setColor(color)
         display.show(Image.YES)
-        # sleep(86 * 1000)
+        sleep(86 * 1000)
         self._last_odom_time = self._current_time()
         a = True
         while a:
+            now = self._current_time()
+            dt  = now - self._last_odom_time
             sleep(10)
-            if self.go() or self.emergencyBtn.is_pressed():
+            if self.go(dt) or self.emergencyBtn.is_pressed():
                 self._stop()
                 a = False
             if self.emergencyBtn.is_pressed():
@@ -63,35 +70,20 @@ class Sima:
     def turnServo(self, deg):
         self.wk.set_servo(self.servo_pin, deg)  # Gira el servo a 0 grados (ajusta según necesidad)
 
-    def go(self):
+    def go(self, dt):
         if self.emergencyBtn.is_pressed():
             self._stop()
             display.show(Image.NO)
             return True # Break loop principal
 
-        self._update_odometry()
-        distance_obst = self._read_ultrasonic()
-
-        if self.state == "GO_TO_GOAL":
-            if 0 < distance_obst < self.ULTRASONIC_THRESHOLD:
-                # Obstáculo detectado → cambiar a modo esquiva
-                self._stop()
-                display.show(Image.SAD)
-                self.state = "AVOID_OBSTACLE"
-            else:
-                reached_goal = self._follow_point(self.xf, self.yf)
-                if reached_goal:
-                    self._stop()
-                    display.show(Image.HAPPY)
-                    return True # Objetivo alcanzado, salir del loop principal
-
-        elif self.state == "AVOID_OBSTACLE":
-            self._avoid_obstacle_maneuver()
-            # Tras la maniobra volvemos a navegar hacia el objetivo
-            self.state = "GO_TO_GOAL"
-            display.show(Image.ARROW_N)
-
-        return False # Continuar el loop principal
+        if dt <= 5000:
+            self.wk.set_motors(1, 100)
+            self.wk.set_motors(2, -100)
+            return False
+        else:    
+            return True
+                
+                
 
     def _dataToVal(self, data):
         if data == "F" or data == 0:
@@ -99,6 +91,8 @@ class Sima:
         else:
             return True
 
+
+        
     def _stop(self):
         self.wk.set_motors(1, 0)
         self.wk.set_motors(2, 0)
@@ -106,11 +100,9 @@ class Sima:
     
 if __name__ == '__main__':
     SIMA = Sima(
-        x0 = 50.0, y0 = 1950.0, firstAngle = 180.0,
-        xf = 50.0, yf = 800.0,
-        v = 40.816327, angularVelocity = 500.0,
-        lin_regulation = 100.0, ang_regulation = 100.0,
         HC_SR04_pin = pin2, panic_pin = pin1,
         channel = 36, servo_pin=0
     )
+    SIMA.start()
+
     SIMA.start()
